beab360249f4a837c34141711a5d29357ba77b47
Junior Conte <juniorcnt@gmail.com>
2017-03-27 23:27:59 -0300

    ADD: Yo Angular Generate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blackjackApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blackjackApp
+ */
+angular.module('blackjackApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
